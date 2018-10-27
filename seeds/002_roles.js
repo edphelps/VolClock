b81a985f0f201349a7c61fc49fee79c04068e865
BrettBlox <Diamond5.12>
2018-10-27 14:39:06 -0600

    added users_roles migration and seed and added data into roles seed file

--- seeds/002_roles.js
+++ seeds/002_roles.js
@@ -1,13 +1,24 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('roles').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('roles').insert([
+        {id: 1, role: 'Cashier'},
+        {id: 2, role: 'Floor'},
+        {id: 3, role: 'Hardgoods'},
+        {id: 4, role: 'Backroom'},
+        {id: 5, role: 'Shoes'},
+        {id: 6, role: 'Boutique'},
+        {id: 7, role: 'Activewear'},
+        {id: 8, role: 'Electronics'},
+        {id: 9, role: 'Clothing'},
+        {id: 10, role: 'Books'},
+        {id: 11, role: 'Cards'},
+        {id: 12, role: 'Linens'},
+        {id: 13, role: 'Toys'},
+        {id: 14, role: 'Other'}
       ]);
     });
 };
