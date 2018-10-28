e6e2e33cde9dc1a6c6c3a9b8d3dc2469c4e33cc9
BrettBlox <Diamond5.12>
2018-10-28 10:38:23 -0600

    trying to migrate

--- migrations/20181027122912_users.js
+++ migrations/20181027122912_users.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
 		table.varchar(`fname`, 255).notNullable()
 		table.varchar(`lname`, 255).notNullable()
 		table.varchar(`login_code`, 4).notNullable()
-    table.integer(`miles_default`).primary().defaultTo(0)
+    table.integer(`miles_default`).defaultTo(0)
 		table.timestamps(true, true)
 	})
 }
