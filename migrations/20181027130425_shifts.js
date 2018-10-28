2ddfdf6a62fe38ef8645f793720caa29094bcf0e
BrettBlox <Diamond5.12>
2018-10-27 19:26:43 -0600

    shifts migration now references miles_default in users table

--- migrations/20181027130425_shifts.js
+++ migrations/20181027130425_shifts.js
@@ -2,11 +2,11 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable('shifts', function(table) {
 		// TABLE COLUMN DEFINITIONS HERE
 		table.increments().primary()
-		table.integer('user_id').references('id').inTable('users').notNullable().onDelete('cascade')
-		table.integer('role_id').references('id').inTable('roles').notNullable().onDelete('cascade')
+		table.integer('user_id').references('id').inTable('users').notNullable()
+		table.integer('role_id').references('id').inTable('roles').notNullable()
     table.datetime('start').notNullable()
     table.datetime('end').notNullable()
-    table.integer('miles').notNullable().defaultTo(0)
+    table.integer('miles').references('miles_default')inTable('users')
 		table.timestamps(true, true)
 	})
 }
