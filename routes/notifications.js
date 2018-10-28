3cd700e333aac25bda6059372ec07e5f87fcfb3d
BrettBlox <Diamond5.12>
2018-10-28 17:26:33 -0600

    notifications post route added

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const knex = require('../knex')
 
 const router = express.Router();
 
 /* **************************************************
 
 ***************************************************** */
-router.get('/', (req, res, next) => {
-  res.send('Notificartions responds with a resource');
-});
+router.post('/', (req, res, next) => {
+  knex('notifications')
+      .insert({
+        'user_id': req.body.user_id,
+        'start': req.body.start,
+        'end': req.body.end,
+        'comment': req.body.comment
+      })
+      .returning(['user_id', 'start', 'end', 'comment'])
+      .then((data) => {
+        res.status(200).json({ message: "success!", posted_data: data[0]})
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    })
+
+
 
 module.exports = router;
