dc2cbc40fde24bf9a82971ac211b75f614212146
Ed Phelps <edphelps1@gmail.com>
2018-10-28 12:31:59 -0600

    cleaning up route handling
    - Minor changes to cleanup the initial route handling from the iniital express-generate.
    - deleted reoutes/index.js since we get index.html directly form public folder
    - Deleted test.html and ztest.html as these are only used to figure out Team github use.

--- routes/notifications.js
+++ routes/notifications.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET users listing. */
+/* **************************************************
+*  GET /users/:login/:login_code
+*  Return:
+*     200: { user: { id, fname, ...}  // if login_code found
+*     404: { user: null } // if login_code not found
+http GET localhost:3000/users/:login_code
+***************************************************** */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
