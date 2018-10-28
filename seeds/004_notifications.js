670a0504b54fb5d19e71fd46622f0e574532656c
BrettBlox <Diamond5.12>
2018-10-27 19:21:42 -0600

    notifications seeds

--- seeds/004_notifications.js
+++ seeds/004_notifications.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('notifications').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('notifications').insert([
+        {id: 1, start: new Date(), end: new Date(), comment: "Riding orcas in Cape Town that week."},
+        {id: 2, start: new Date(), end: new Date(), comment: "Going on vacation."},
+        {id: 3, start: new Date(), end: new Date(), comment: "Trump rally."},
+        {id: 4, start: new Date(), end: new Date(), comment: "Square dance convention for three days."},
+        {id: 5, start: new Date(), end: new Date(), comment: "BURNING MAN."}
       ]);
     });
 };
