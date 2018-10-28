ea0e8bfd0fcb253a3709ff196f3fd405b75ac4c7
Ed Phelps <edphelps1@gmail.com>
2018-10-28 11:30:46 -0600

    Server is running
    Commented out three routes that we added as they were causing the server to fail to start.  Will not start building up the routes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var tablenameRouter = require(`./routes/tablename`)
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var tablenameRouter = require(`./routes/tablename`)
 
 var app = express();
 
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use(`/`, tablenameRouter)
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use(`/`, tablenameRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
