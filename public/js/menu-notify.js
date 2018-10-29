01c2c0d75107c68beeb361e838064c5fd000cd04
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-10-29 12:55:54 -0600

    Basic button events set up
    basic event handlers set up for notify section. No axios yet.

--- public/js/menu-notify.js
+++ public/js/menu-notify.js
@@ -4,31 +4,52 @@
 *
 *  Menu selection
 * =================================================== */
+
 function onMenuNotify() {
   changeMenuAndContentArea("nav--notify", gelemContentNotify);
 }
+
 const timeOffButton = document.getElementById('time-off-button')
 const timeOffForm = document.getElementById('inputTimeOffForm')
 const notifySupervisorButton = document.getElementById('notify-button')
 const notifyForm = document.getElementById('contactSupervisorForm')
 const buttonOptions = document.getElementById('contactButtons')
 
+//functions
+notifySupervisorClick()
+notifySupervisorPost()
+timeOffClick()
+timeOffPost()
+
 //When Notify button clicked, notify form appears
-notifySupervisorButton.addEventListener('click', (ev) => {
-  buttonOptions.style.display="none"
-  notifyForm.style.display="block"
-})
-
+function notifySupervisorClick() {
+  notifySupervisorButton.addEventListener('click', (ev) => {
+    buttonOptions.style.display="none"
+    notifyForm.style.display="block"
+  })
+}
 //Notify Form functionality
-notifyForm.addEventListener('submit', (ev) => {
-  ev.preventDefault()
-  //POST request goes Here
-  notifyForm.style.display="none"
-  buttonOptions.style.display=""
-})
-
+function notifySupervisorPost(){
+  notifyForm.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+    //POST request goes Here
+    notifyForm.style.display="none"
+    buttonOptions.style.display=""
+  })
+}
 //time off button click, time off form appears
-timeOffButton.addEventListener('click', (ev) => {
-  buttonOptions.style.display="none"
-  timeOffForm.style.display="inline"
-})
+function timeOffClick() {
+  timeOffButton.addEventListener('click', (ev) => {
+    buttonOptions.style.display="none"
+    timeOffForm.style.display="inline"
+  })
+}
+//time off form functionality
+function timeOffPost() {
+  timeOffForm.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+    //POST request goes here!
+    timeOffForm.style.display="none"
+    buttonOptions.style.display=""
+  })
+}
