eed486ac4fef962e0c535939c3678aa2d8155742
Ed Phelps <edphelps1@gmail.com>
2018-10-29 15:19:49 -0600

    added error handling

--- routes/roles.js
+++ routes/roles.js
@@ -1,16 +1,23 @@
 const express = require('express');
-const router = express.Router();
 const knex = require('../knex')
 
-/* GET users listing. */
+const routeCatch = require('./routeCatch');
+const router = express.Router();
+
+/* **************************************************
+*  GET /
+*  Get all roles
+*  Return
+*    200: { notifications: [ { id, user_id, start_date, ... }, { id, user_id, start_date, ... } ]
+http GET localhost:3000/roles/
+***************************************************** */
 router.get('/', (req, res, next) => {
   knex('roles')
     .then((results) => {
-      res.json({ roles: results })
+      res.status(200).json({ roles: results });
     })
     .catch((error) => {
-     console.log("%%% knex:readAll error :", error);
-    throw new Error(error.message); // send to caller's .catch()
-    })
-})
+      next(routeCatch(`--- GET roles/ route`, error));
+    });
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,73 @@
-const express = require('express')
-const router = express.Router()
-const knex = require('../knex')
+const express = require('express');
 
-/* GET users listing. */
+const router = express.Router();
+const knex = require('../knex');
+const routeCatch = require('./routeCatch');
+
+/* **************************************************
+*  GET /login/:login_code
+*  Lookup login_code:
+*     If found, return user object
+*     If not found, return user = "null"
+*  Return
+*    200 { user: { id, fname, ... } }
+*    200 { user: "null" }
+http GET localhost:3000/users/login/0000
+***************************************************** */
 router.get('/login/:login_code', (req, res, next) => {
   knex('users')
     .where('login_code', req.params.login_code)
     .then((result) => {
-      if (!result.length) { res.status(200).json({ user: null })}
-      res.json({ user: result[0] })
+      if (!result.length) {
+        res.status(200).json({ user: "null" });
+        return;
+      }
+      res.json({ user: result[0] });
     })
     .catch((error) => {
-     console.log("%%% knex:readAll error :", error);
-    throw new Error(error.message); // send to caller's .catch()
-   })
+      next(routeCatch(`--- GET /login/${req.params.user_id} route`, error));
+    });
 })
 
-router.get('/:user_id', (req, res, next) => {
+/* **************************************************
+*  GET /:id
+*  Lookup login_code:
+*     If found, return user object
+*     If not found, return user = "null"
+*  Return
+*    200 { user: { id, fname, ... } }
+*    200 { user: "null" }
+http GET localhost:3000/users/2
+***************************************************** */
+router.get('/:id', (req, res, next) => {
   let userRoles = {
     user: {},
     roles: [],
   }
   knex('users')
-    .where('users.id', req.params.user_id)
+    .where('users.id', req.params.id)
     .returning('*')
     .then((user) => {
+      if (!user.length) {
+        console.log(`--- users get ${req.params.id} -- rec not found`);
+        const error = new Error(`unable to get user ${req.params.id}, not found`);
+        error.status = 404;
+        throw error;
+      }
       knex('users_roles')
         .join('roles', 'roles.id', 'users_roles.role_id')
-        .where('user_id', req.params.user_id)
+        .where('user_id', req.params.id)
         .returning(['roles.id', 'roles.role'])
         .then((result) => {
-              res.status(200).send({ user: user[0], roles: result })
+          res.status(200).send({ user: user[0], roles: result })
         })
+        .catch((error) => {
+          next(routeCatch(`--- GET /user/${req.params.user_id} route`, error));
+        });
     })
+    .catch((error) => {
+      next(routeCatch(`--- GET /user/${req.params.user_id} route`, error));
+    });
 })
 
 
