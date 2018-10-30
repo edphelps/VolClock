48f29a880fbdf52708ccdf59452e32dc22d5d3d5
Ed Phelps <edphelps1@gmail.com>
2018-10-30 12:24:59 -0600

    refactored then chain

--- routes/users.js
+++ routes/users.js
@@ -32,41 +32,42 @@ router.get('/login/:login_code', (req, res, next) => {
 *     If found, return user object
 *     If not found, return user = "null"
 *  Return
-*    200 { user: { id, fname, ... } }
-*    200 { user: "null" }
+*    200: {
+        user: { id, fname, ... },
+        roles: [ { id, role }, { id, role } ] ,
+        last_role: { id, role }
+}
 http GET localhost:3000/users/2
 ***************************************************** */
 router.get('/:id', (req, res, next) => {
-  let userRoles = {
-    user: {},
-    roles: [],
-  }
+  let saveUser = null;
   knex('users')
     .where('users.id', req.params.id)
     .returning('*')
     .then((user) => {
+      // console.log("GET -- user: ", user);
       if (!user.length) {
         console.log(`--- users get ${req.params.id} -- rec not found`);
         const error = new Error(`unable to get user ${req.params.id}, not found`);
         error.status = 404;
         throw error;
       }
-      knex('users_roles')
+      saveUser = user[0];
+    })
+    .then(() => {
+      // console.log("GET -- getting user roles");
+      return knex('users_roles')
         .join('roles', 'roles.id', 'users_roles.role_id')
         .where('user_id', req.params.id)
-        .returning(['roles.id', 'roles.role'])
-        .then((result) => {
-          res.status(200).send({ user: user[0], roles: result })
-        })
-        .catch((error) => {
-          next(routeCatch(`--- GET /user/${req.params.user_id} route`, error));
-        });
+        .returning(['roles.id', 'roles.role']);
+    })
+    .then((result) => {
+      // console.log("--- GET resut of roles: ", result);
+      res.status(200).send({ user: saveUser, roles: result });
     })
     .catch((error) => {
       next(routeCatch(`--- GET /user/${req.params.user_id} route`, error));
     });
-})
-
-
+});
 
 module.exports = router;
