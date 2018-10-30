0dd51c4f320ebf4c7583e067257ce90237766615
TaylorArmstrong <TArmstrong1327@gmail.com>
2018-10-30 13:46:39 -0600

    shift history table working

--- public/js/menu-history.js
+++ public/js/menu-history.js
@@ -1,4 +1,6 @@
-
+// document.addEventListener('DOMContentLoaded', () => {
+//   onMenuHistory()
+// })
 
 /* ==================================================
 *  onMenuHistory()
@@ -13,19 +15,36 @@ function onMenuHistory() {
       if(response.data.shifts.length === 0) {
         res.status(201).json({ message: 'success' })
       }
+
+      let shiftHistoryList = document.getElementById('list-history')
+      let table = document.createElement('table')
+      let tableHead = document.createElement('thead')
+      let tableBody = document.createElement('tbody')
+
+        tableHead.innerHTML += `
+          <tr>
+            <th scope="col">Role Id</th>
+            <th scope="col">Start Time</th>
+            <th scope="col">End Time</th>
+            <th scope="col">Miles</th>
+          </tr>`
+
       response.data.shifts.forEach((shift) => {
-        let shiftHistoryList = document.querySelector('tbody.shiftHistory')
-        while(shiftHistory.firstChild) {
-          shiftHistory.removeChild(shiftHistory.firstChild)
-        }
-        const row = document.createElement('tr')
-        const body = ``
+        console.log('shift>>>>', shift)
+        let shiftRow = document.createElement('tr')
+
+        shiftRow.innerHTML += `<td>${shift.role_id}</td>
+          <td>${shift.start_time}</td>
+          <td>${shift.end_time}</td>
+          <td>${shift.miles}</td>`
+
+      tableBody.appendChild(shiftRow)
       })
-      console.log('response.data.shifts>>>', response.data.shifts)
+      table.appendChild(tableHead)
+      table.appendChild(tableBody)
+      shiftHistoryList.appendChild(table)
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error)
     })
   }
-
-onMenuHistory()
