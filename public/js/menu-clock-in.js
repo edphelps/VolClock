96cb6f29a4bd68ced5d2c1bb90da83cd39dec29f
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-10-30 13:57:11 -0600

    Working on Post request clock-in

--- public/js/menu-clock-in.js
+++ public/js/menu-clock-in.js
@@ -6,8 +6,8 @@
 * =================================================== */
 function onMenuClockIn() {
   changeMenuAndContentArea("nav--clock-in", gelemContentClockIn);
-  checkStatus()
-
+  // checkStatus()
+  clockIn()
 }
 
 const clockInDiv = document.getElementById('clockInDiv')
@@ -26,11 +26,13 @@ function getRoles() {
 
   //loop over roles array and create buttons with role names
   for(let el of roles) {
+    // console.log(el.id)
       let clockInButton = document.createElement('button')
       clockInButton.innerText = el.role
       clockInDiv.appendChild(clockInButton)
       clockInButton.setAttribute('class', "btn btn-primary btn-lg btn-block")
       clockInButton.setAttribute('type', "button")
+      clockInButton.setAttribute('id', el.id)
     }
   })
   .catch((error) => {
@@ -39,16 +41,39 @@ function getRoles() {
 }
 
 // check if user is already clocked in
-function checkStatus() {
-  axios.get(`shifts/user/${gactiveUserId}/current`)
+// function checkStatus() {
+//   axios.get(`shifts/user/${gactiveUserId}/current`)
+//
+//   .then((shift) => {
+//     if(shift.data.current_shift.end_time === null){
+//       clockInDiv.style.display = "none"
+//       clockOutDiv.style.display = "inline-block"
+//     }
+//   })
+//   .catch((eror) => {
+//     console.log(error)
+//   })
+// }
 
-  .then((shift) => {
-    if(shift.data.current_shift.end_time === null){
-      clockInDiv.style.display = "none"
-      clockOutDiv.style.display = "inline-block"
-    }
-  })
-  .catch((eror) => {
-    console.log(error)
-  })
+function clockIn() {
+  //event listener on clock in buttons div
+  clockInDiv.addEventListener('click', (ev) => {
+    let mileage = parseInt(milesInput.value)
+    let roleId = parseInt(ev.target.id)
+
+    let dataObject = {}
+    dataObject['user_id'] = gactiveUserId
+    dataObject['role_id'] = roleId
+    dataObject['miles'] = mileage
+    console.log(dataObject);
+
+    axios.post(`/shifts`, dataObject)
+      .then((post) => {
+        console.log(post)
+      })
+    })
+
+
+
+  //post request to shifts table with user_id, role_id, and miles
 }
