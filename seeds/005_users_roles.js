bedbcba1184ae23fbeb5915f61d1a0d7bc846781
BrettBlox <Diamond5.12>
2018-10-30 14:15:36 -0600

    users_roles seeds added

--- seeds/005_users_roles.js
+++ seeds/005_users_roles.js
@@ -6,6 +6,11 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('users_roles').insert([
         {id: 1, user_id: 1, role_id: 1},
+        {id: 2, user_id: 2, role_id: 2},
+        {id: 3, user_id: 3, role_id: 3},
+        {id: 4, user_id: 4, role_id: 4},
+        {id: 5, user_id: 4, role_id: 12},
+
       ])
       .then(() => {
 				 // Moves id column (PK) auto-incremented to correct value after inserts
