44b9a6b9824bbd362c056bbec0ef081e1ca9ff93
BrettBlox <Diamond5.12>
2018-10-30 14:02:08 -0600

    edited shifts seeds

--- seeds/003_shifts.js
+++ seeds/003_shifts.js
@@ -6,14 +6,6 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('shifts').insert([
         {id: 1, user_id: 1, role_id: 4, start_time: new Date(), end_time: new Date(), miles: 3},
-        {id: 2, user_id: 2, role_id: 3, start_time: new Date(), end_time: new Date(), miles: 4},
-        {id: 3, user_id: 3, role_id: 2, start_time: new Date(), end_time: new Date(), miles: 5},
-        {id: 4, user_id: 4, role_id: 1, start_time: new Date(), end_time: new Date(), miles: 6},
-        {id: 5, user_id: 2, role_id: 4, start_time: new Date(), end_time: null,       miles: 4},
-        {id: 6, user_id: 2, role_id: 5, start_time: new Date(), end_time: null,       miles: 4},
-        {id: 7, user_id: 2, role_id: 6, start_time: new Date(), end_time: null,       miles: 4},
-        {id: 8, user_id: 2, role_id: 7, start_time: new Date(), end_time: null,       miles: 4},
-        {id: 9, user_id: 2, role_id: 8, start_time: new Date(), end_time: null,       miles: 4},
       ])
       .then(() => {
                  // Moves id column (PK) auto-incremented to correct value after inserts
