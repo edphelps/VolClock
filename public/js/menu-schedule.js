8e9503cfa1a484bf677c2dc2d2294c89669fd7bc
Ed Phelps <edphelps1@gmail.com>
2018-10-31 15:42:56 -0600

    assignments basic functionality
    Added the seeds and the basic ability to see the times on the assignment page.  Still need to sort the assignments in case more than one in a day and get the role names to show.

--- public/js/menu-schedule.js
+++ public/js/menu-schedule.js
@@ -1,4 +1,21 @@
+/* ------------------
+*  getTimeOnly()
+*  in the form:  9:00a  2:00p
+--------------------- */
+function getTimeOnly(_dt) {
+  const dt = new Date(_dt); // this allows the dt param to be Date or String
+  if (isNaN(dt)) {
+    return "?";
+  }
+  let hrs = dt.getHours() + 1;
+  let am = (hrs <= 12) ? true : false;
+  if (12 < hrs) hrs -= 12;
+  const mins = dt.getMinutes();
 
+  return `${hrs}:${(mins < 10 ? '0' : '')}${mins}${(am) ? 'a' : 'p'}`;
+  // return `${dt.getHours() + 1}:${dt.getMinutes()}`;
+}
+// ------------------
 
 /* ==================================================
 *  onMenuSchedule()
@@ -7,4 +24,29 @@
 * =================================================== */
 function onMenuSchedule() {
   changeMenuAndContentArea("nav--schedule", gelemContentSchedule);
+
+  axios.get(`assignments/user/${gactiveUserId}`)
+    .then((res) => {
+      console.log("res: ", res);
+      const aAssignments = res.data.assignments;
+
+      for (let dow = 0; dow <= 6; dow++) {
+        const elemDow = document.getElementById(`assignment-${dow}`);
+        elemDow.innerHTML = "";
+
+        for (const assignment of aAssignments) {
+          if (assignment.dow === dow) {
+            elemDow.innerHTML += `
+               ${assignment.role_id}<br>
+               ${getTimeOnly(assignment.start_time)}<br>
+               |<br>
+               ${getTimeOnly(assignment.end_time)}`;
+          }
+        }
+      }
+    })
+    .catch((error) => {
+      handleError("renderRecentRequests", error);
+    });
+
 }
--- seeds/006_assignments.js
+++ seeds/006_assignments.js
@@ -5,8 +5,8 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       // Inserts seed entries
       return knex('assignments').insert([
-        {id: 1, user_id: 2, role_id: 4, start_time: new Date('2000-01-01 14:00:00 UTC'), end_time: new Date('2000-01-01 14:30:00 UTC'), dow: 3},
-        {id: 2, user_id: 2, role_id: 3, start_time: new Date('2000-01-01 9:15:00 UTC'), end_time: new Date('2000-01-01 12:30:00 UTC'), dow: 1},
+        {id: 1, user_id: 2, role_id: 4, start_time: new Date('2000-01-01 18:00:00 UTC'), end_time: new Date('2000-01-01 20:30:00 UTC'), dow: 3},
+        {id: 2, user_id: 2, role_id: 3, start_time: new Date('2000-01-01 15:15:00 UTC'), end_time: new Date('2000-01-01 18:30:00 UTC'), dow: 1},
       ])
       .then(() => {
         // Moves id column (PK) auto-incremented to correct value after inserts
