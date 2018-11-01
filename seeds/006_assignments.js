c6c2e2e89cd31f88efa5f57366212966f9e1bacc
Ed Phelps <edphelps1@gmail.com>
2018-11-01 08:58:57 -0600

    fixed bad seed

--- seeds/006_assignments.js
+++ seeds/006_assignments.js
@@ -5,9 +5,9 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       // Inserts seed entries
       return knex('assignments').insert([
-        {id: 1, user_id: 2, role_id: 4, start_time: new Date('2000-01-01 18:00:00 UTC'), end_time: new Date('2000-01-01 20:30:00 UTC'), dow: 3},
-        {id: 2, user_id: 2, role_id: 4, start_time: new Date('2000-01-01 16:45:00 UTC'), end_time: new Date('2000-01-01 18:00:00 UTC'), dow: 3},
-        {id: 2, user_id: 2, role_id: 3, start_time: new Date('2000-01-01 15:15:00 UTC'), end_time: new Date('2000-01-01 18:30:00 UTC'), dow: 1},
+        { id: 1, user_id: 2, role_id: 2, start_time: new Date('2000-01-01 18:00:00 UTC'), end_time: new Date('2000-01-01 20:30:00 UTC'), dow: 3 },
+        { id: 2, user_id: 2, role_id: 4, start_time: new Date('2000-01-01 16:45:00 UTC'), end_time: new Date('2000-01-01 18:00:00 UTC'), dow: 3 },
+        { id: 3, user_id: 2, role_id: 3, start_time: new Date('2000-01-01 15:15:00 UTC'), end_time: new Date('2000-01-01 18:30:00 UTC'), dow: 1 },
       ])
       .then(() => {
         // Moves id column (PK) auto-incremented to correct value after inserts
